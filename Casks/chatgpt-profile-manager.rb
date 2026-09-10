48d1fc2dc102b9d72c08d5c47d7b374965ead977
octane96 <keitaokuda.0925@gmail.com>
2026-09-10 19:02:35 +0900

    chore: update cask to 1.1.3 (#4)

--- Casks/chatgpt-profile-manager.rb
+++ Casks/chatgpt-profile-manager.rb
@@ -1,6 +1,6 @@
 cask "chatgpt-profile-manager" do
-  version "1.1.2"
-  sha256 "f9f9061e31edf42af8d28bd07c64f2bcceeb695df571c4816e0d10e27b700171"
+  version "1.1.3"
+  sha256 "494f7324b7945fb50121b286c8b375def07a3773e1e08fb0107e7acffb5475dd"
 
   url "https://github.com/octane96/ChatGPT-Profile-Manager-for-Mac/releases/download/v#{version}/ChatGPT-Profile-Manager-macOS.zip"
   name "ChatGPT Profile Manager"
